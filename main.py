be20b731efd90273a75effe7558c6ce191c90a26
ecorporation <lajos.turi3@gmail.com>
2024-03-11 17:08:36 +0100

    the symbol/ask is implemented with mt5

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask, redirect, url_for, render_template, request
-import yfinance as yf
+import MetaTrader5 as mt5
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    mt5.initialize()
+    idx = 0
+    prices = []
+    if(request.method == "POST"):
+        symbol = request.form["symbol"]
+        price = request.form["price"]
+        try:
+            cp = mt5.symbol_info_tick(symbol)._asdict()['ask']
+            print(cp)
+            return render_template("index.html", symbol=symbol, cp=cp)
+        except:
+            print(f"Error downloading data for {symbol}...")
+        return render_template("index.html", symbol=symbol, cp="N/A")
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
