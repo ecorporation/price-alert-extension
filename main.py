b39103f3ee5063e7b5d09d1ea3aa3956ba4e583a
ecorporation <lajos.turi3@gmail.com>
2024-03-11 17:24:14 +0100

    async function created

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from flask import Flask, redirect, url_for, render_template, request
 import MetaTrader5 as mt5
+import time
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    mt5.initialize()
-    idx = 0
-    prices = []
     if(request.method == "POST"):
         symbol = request.form["symbol"]
         price = request.form["price"]
         try:
             cp = mt5.symbol_info_tick(symbol)._asdict()['ask']
-            print(cp)
+            alert()
             return render_template("index.html", symbol=symbol, cp=cp)
         except:
             print(f"Error downloading data for {symbol}...")
         return render_template("index.html", symbol=symbol, cp="N/A")
     else:
+        mt5.initialize()
         return render_template("index.html")
 
+async def alert():
+    while(True):
+        '''
+            NOT IMPLEMENTED YET
+        '''
+        time.sleep(10)
+        
 if __name__ == '__main__':
     app.run(debug=True, port=80)
