e3a2e1f663beac57ff8dacc271758ac9e4ce985d
ecorporation <lajos.turi3@gmail.com>
2024-03-11 15:51:04 +0100

    html rendered

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Put something here..."
+    return render_template("index.html")
 
-app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
